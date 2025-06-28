c67015364be3b8af47055cd5bb3ebb0909e79e74
Gotze <thorholby@gmail.com>
2025-06-29 01:41:18 +0200

    Make CurrentSoundData final and refactor volume and pitch adjustment methods

--- src/main/java/com/gotze/blockbreaksounds/model/CurrentSoundData.java
+++ src/main/java/com/gotze/blockbreaksounds/model/CurrentSoundData.java
@@ -1,15 +1,11 @@
 package com.gotze.blockbreaksounds.model;
 
 import com.gotze.blockbreaksounds.BlockBreakSoundsPlugin;
-import com.gotze.blockbreaksounds.util.GUIUtils;
-import org.bukkit.ChatColor;
+import com.gotze.blockbreaksounds.util.SoundUtils;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,57 +13,68 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
-import static com.gotze.blockbreaksounds.util.ItemStackCreator.createItemStack;
-
-public class CurrentSoundData extends SoundData {
+public final class CurrentSoundData extends SoundData {
 
     public static final Map<UUID, SoundData> currentSound = new HashMap<>();
 
-    public CurrentSoundData(Sound sound, float volume, float pitch, Material material) {
-        super(sound, volume, pitch, material);
+    private CurrentSoundData(Sound sound, float volume, float pitch, Material displayMaterial) {
+        super(sound, volume, pitch, displayMaterial);
     }
 
-    public static void setCurrentSound(Player player, SoundData soundData) {
-        player.playSound(player, soundData.getSound(), soundData.getVolume(), soundData.getPitch());
-        currentSound.put(player.getUniqueId(), soundData);
+    public static void increaseVolume(Player player) {
+        SoundData currentSoundData = currentSound.get(player.getUniqueId());
+        if (currentSoundData == null) {
+            SoundUtils.playErrorSound(player);
+            return;
+        }
+
+        currentSoundData.setVolume(Math.round((currentSoundData.getVolume() + 0.05f) * 100f) / 100f);
+        currentSoundData.playSoundData(player);
+        currentSound.put(player.getUniqueId(), currentSoundData);
         saveCurrentSoundDataToYAML(player);
     }
 
-    public static void clearCurrentSound(Inventory gui, Player player, int slot) {
-        ItemStack clickedItem = gui.getItem(slot);
-        if (clickedItem == null) return;
-
-        Material materialOfSound = clickedItem.getType();
-
-        if (materialOfSound == Material.GLASS_PANE) {
-            player.playSound(player, Sound.BLOCK_NOTE_BLOCK_BASS, 0.5f, 0.5f);
+    public static void decreaseVolume(Player player) {
+        SoundData currentSoundData = currentSound.get(player.getUniqueId());
+        if (currentSoundData == null || currentSoundData.getVolume() == 0.00f) {
+            SoundUtils.playErrorSound(player);
             return;
         }
 
-        if (materialOfSound != Material.BARRIER) {
-            ItemStack confirmClearCurrentSoundItem = createItemStack(Material.BARRIER, ChatColor.RED + "ᴅʀᴏᴘ ᴀɢᴀɪɴ ᴛᴏ ᴄʟᴇᴀʀ");
-            gui.setItem(slot, confirmClearCurrentSoundItem);
+        if (currentSoundData.getVolume() == 0.05f ) {
+            SoundUtils.playErrorSound(player);
+        }
 
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    ItemStack itemStackAfterDelay = gui.getItem(slot);
-                    if (itemStackAfterDelay == null) return;
+        currentSoundData.setVolume(Math.round((currentSoundData.getVolume() - 0.05f) * 100f) / 100f);
+        currentSoundData.playSoundData(player);
+        currentSound.put(player.getUniqueId(), currentSoundData);
+        saveCurrentSoundDataToYAML(player);
+    }
 
-                    if (itemStackAfterDelay.getType().equals(Material.BARRIER)) {
-                        gui.setItem(slot, clickedItem);
-                    }
-                }
-            }.runTaskLater(BlockBreakSoundsPlugin.INSTANCE, 60L);
+    public static void increasePitch(Player player) {
+        SoundData currentSoundData = CurrentSoundData.currentSound.get(player.getUniqueId());
+        if (currentSoundData == null) {
+            SoundUtils.playErrorSound(player);
             return;
         }
 
-        if (materialOfSound == Material.BARRIER) {
-            player.playSound(player, Sound.BLOCK_NOTE_BLOCK_BASS, 0.5f, 0.5f);
-            currentSound.remove(player.getUniqueId());
-            gui.setItem(slot, GUIUtils.CurrentSoundDisplayButton(player));
-            saveCurrentSoundDataToYAML(player);
+        currentSoundData.setVolume(Math.round((currentSoundData.getPitch() + 0.05f) * 100f) / 100f);
+        currentSoundData.playSoundData(player);
+        currentSound.put(player.getUniqueId(), currentSoundData);
+        saveCurrentSoundDataToYAML(player);
+    }
+
+    public static void decreasePitch(Player player) {
+        SoundData currentSoundData = CurrentSoundData.currentSound.get(player.getUniqueId());
+        if (currentSoundData == null || currentSoundData.getPitch() == 0.50f) {
+            SoundUtils.playErrorSound(player);
+            return;
         }
+
+        currentSoundData.setVolume(Math.round((currentSoundData.getPitch() - 0.05f) * 100f) / 100f);
+        currentSoundData.playSoundData(player);
+        currentSound.put(player.getUniqueId(), currentSoundData);
+        saveCurrentSoundDataToYAML(player);
     }
 
     public static void saveCurrentSoundDataToYAML(Player player) {
@@ -93,23 +100,6 @@ public class CurrentSoundData extends SoundData {
             yamlConfiguration.save(playerFile);
         } catch (IOException e) {
             e.printStackTrace();
-        }
-    }
-
-    public static void loadCurrentSoundDataFromFile(Player player) {
-        File playerFile = new File(BlockBreakSoundsPlugin.INSTANCE.getDataFolder() + "/playerdata", player.getUniqueId() + ".yml");
-        YamlConfiguration yamlConfiguration = YamlConfiguration.loadConfiguration(playerFile);
-        String path = "current-sound";
-
-        try {
-            Sound sound = Sound.valueOf(yamlConfiguration.getString(path + ".sound"));
-            float volume = (float) yamlConfiguration.getDouble(path + ".volume");
-            float pitch = (float) yamlConfiguration.getDouble(path + ".pitch");
-            Material material = Material.valueOf(yamlConfiguration.getString(path + ".material"));
-
-            currentSound.put(player.getUniqueId(), new SoundData(sound, volume, pitch, material));
-        } catch (Exception e) {
-            System.out.println("Failed to load current sound for " + player.getName() + e.getMessage());
         }
     }
 }
